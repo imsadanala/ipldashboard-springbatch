bccb382a337cc2e0ce73c41ae71e0f3e94f2eb11
imsadanala <sadanalansp@gmail.com>
2021-04-18 16:14:14 +0530

    Batch Job & Step & Notification changes

--- src/main/java/io/sadanala/ipldashboard/batch/BatchConfig.java
+++ src/main/java/io/sadanala/ipldashboard/batch/BatchConfig.java
@@ -2,9 +2,12 @@ package io.sadanala.ipldashboard.batch;
 
 import javax.sql.DataSource;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -54,4 +57,15 @@ public class BatchConfig {
                 .dataSource(dataSource).build();
     }
 
+    @Bean
+    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
+        return jobBuilderFactory.get("importUserJob").incrementer(new RunIdIncrementer()).listener(listener).flow(step1)
+                .end().build();
+    }
+
+    @Bean
+    public Step step1(JdbcBatchItemWriter<Match> writer) {
+        return stepBuilderFactory.get("step1").<MatchInput, Match>chunk(10).reader(reader()).processor(processor())
+                .writer(writer).build();
+    }
 }
